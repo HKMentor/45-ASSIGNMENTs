e5ce5d79e84dc9e211c9aefbe8c8b9d0b9b72ea7
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-03-12 07:48:02 +0500

    question2

---
+++ Assignment2/tech.js
@@ -0,0 +1,4 @@
+//personal message;storea person name in a variable,and print a message to that person your message should be
+//simple such as 'hello eric, woul you like to learnsome phython today
+var hello = 'zainab';
+console.log("hello ".concat(hello, ", would you like to learn some phython today"));
---
+++ Assignment2/tech.ts
@@ -0,0 +1,5 @@
+//personal message;storea person name in a variable,and print a message to that person your message should be
+ //simple such as 'hello eric, woul you like to learnsome phython today
+
+let hello = 'zainab'
+ console.log(`hello ${hello}, would you like to learn some phython today`)
