df360044c2ae79cab75062cbeea23b8ec2622564
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-03-12 07:51:24 +0500

    question3

---
+++ Assignment3/tech.js
@@ -0,0 +1,5 @@
+//3 name cases; store a person name in a variable, and then print that person name in lower case,uppercase and title case.
+var person_name = 'dawood';
+//console.log(person_name.toLowerCase())
+//console.log(person_name.toUpperCase())
+console.log(person_name.charAt(0).toUpperCase() + person_name.slice(1));
---
+++ Assignment3/tech.ts
@@ -0,0 +1,16 @@
+//3 name cases; store a person name in a variable, and then print that person name in lower case,uppercase and title case.
+
+let person_name ='dawood'
+//console.log(person_name.toLowerCase())
+//console.log(person_name.toUpperCase())
+
+console.log(person_name.charAt(0).toUpperCase()+ person_name.slice(1))
+
+
+
+
+
+
+
+
+
