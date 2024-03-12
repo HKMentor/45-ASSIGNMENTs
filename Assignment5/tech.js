b492ed570403be6efa357452d9e27a6e070d67fe
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-03-12 07:54:19 +0500

    question5

---
+++ Assignment5/tech.js
@@ -0,0 +1,5 @@
+//famous qoute2; repeat exercise 4, but this time store the famous_person,s name in a variable called famous_name
+//then compose your meesage and store it in a new variable called messsage.print your message
+var famous_person = 'Winston Churchill';
+var qoute = "success is not final, failure is not fatal: it is the courage to continue that counts.";
+console.log("".concat(famous_person, " once said,").concat(qoute));
---
+++ Assignment5/tech.ts
@@ -0,0 +1,6 @@
+//famous qoute2; repeat exercise 4, but this time store the famous_person,s name in a variable called famous_name
+//then compose your meesage and store it in a new variable called messsage.print your message
+
+let famous_person = 'Winston Churchill'
+let qoute ="success is not final, failure is not fatal: it is the courage to continue that counts."
+console.log(`${famous_person} once said,${qoute}`)
