471d12ee220f0cf9fdc22d427efa4ae4161c90af
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-03-12 07:53:05 +0500

    question4

---
+++ Assignment4/tech.ts
@@ -0,0 +1,5 @@
+//4 famous qoute;find a quote from a famousperson you admire. print the quote and the name of its author. your output
+//shoul look something like the folowing, including the quotation marks;
+//albert einstein once said, 'a person who never made a mistake never tried anything new.'
+
+console.log('Winston Churchill once said,"success is not final, failure is not fatal: it is the courage to continue that counts." ')
