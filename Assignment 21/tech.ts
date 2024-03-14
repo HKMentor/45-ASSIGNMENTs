f40146d43915199bdb8fd3905a47861512812b60
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-03-14 23:52:17 +0500

    question 21

---
+++ Assignment 21/tech.ts
@@ -0,0 +1,18 @@
+//Intentional Error: If you haven’t received an array index error
+// in one of your programs yet, try to make one happen. Change an index in one of your
+// programs to produce an index error. 
+//Make sure you correct the error before closing the program.
+
+
+//index error
+let array:boolean[]=[true , false]
+console.log(array)
+
+
+//correction error
+let array1:boolean[]=[true , false]
+console.log(array1)
+
+
+
+
