a549f4bdf452b369f1cb973201555ef3c8cf1808
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-03-14 23:43:05 +0500

    question 15

---
+++ Assignment 15/tech.ts
@@ -0,0 +1,52 @@
+//More Guests: You just found a bigger dinner table, so now more space is available.
+// Think of three more guests to invite to dinner.
+//• Start with your program from Exercise 15.
+// Add a print statement to the end of your program informing people that you found a bigger dinner table.
+
+//• Add one new guest to the beginning of your array.
+
+//• Add one new guest to the middle of your array. • Use append()
+// to add one new guest to the end of your list. 
+//• Print a new set of invitation messages, one for each person in your list.
+
+let guest:string[]=["neha","kanwal" ,"hooria"]
+guest.pop
+console.log(guest)
+guest.push("maryam","hania" ,"mant", "zainab")
+console.log(guest)
+
+guest.unshift("fatima")
+console.log(guest)
+let a2 = guest.splice(2 ,+ "husain","hassan")
+console.log(a2)
+
+console.log(guest[0]+ ":i would be honored if you could join me for dinner.")
+console.log(guest[1]+ ":i would be honored if you could join me for dinner.")
+console.log(guest[2]+ ":i would be honored if you could join me for dinner.")
+console.log(guest[3]+ ":i would be honored if you could join me for dinner.")
+console.log(guest[4]+ ":i would be honored if you could join me for dinner.")
+
+
+//print a new set of invitatation
+//i found a bigger dinner table//
+console.log(guest[0] + "  i would like to invite you at dinner and i found a bigger dinner table")
+console.log(guest[1] + "  i would like to invite you at dinner and i found a bigger dinner table")
+console.log(guest[2] + "  i would like to invite you at dinner and i found a bigger dinner table")
+console.log(guest[3] + "  i would like to invite you at dinner and i found a bigger dinner table")
+console.log(guest[4] + "  i would like to invite you at dinner and i found a bigger dinner table")
+console.log(guest[5] + "  i would like to invite you at dinner and i found a bigger dinner table")
+console.log(guest[6] + "  i would like to invite you at dinner and i found a bigger dinner table")
+console.log(guest[7] + "  i would like to invite you at dinner and i found a bigger dinner table")
+console.log(guest[8] + "  i would like to invite you at dinner and i found a bigger dinner table")
+console.log(guest[9] + "  i would like to invite you at dinner and i found a bigger dinner table")
+
+
+
+
+
+
+
+
+
+
+
