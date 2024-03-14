4c6808c918e5d945631ea2332ff055dec62a73d6
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-03-14 23:22:33 +0500

    question 6

---
+++ Assignment 6/tech.js
@@ -0,0 +1,7 @@
+//store a person,s name with whitespace characters
+var nameWithWhitespace = "\t\t\n abdul moiz \n\n\t";
+//print the name with whitespace
+console.log('nameWithWhitespace');
+//print the stripped name 
+//console.log("stripped name:",
+//nameWithWhitespace.trim())
