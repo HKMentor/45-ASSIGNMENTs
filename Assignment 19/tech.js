32f9bdd09c107acc434f517176dd562a53583336
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-03-14 23:49:30 +0500

    question 19

---
+++ Assignment 19/tech.js
@@ -0,0 +1,33 @@
+//Think of something you could store in a array.
+// For example, you could make a list of mountains, rivers, countries, 
+//cities, languages, or anything else you’d like. Write a program that 
+//creates a list containing these items.
+var languages = ['urdu', 'english', 'chines', 'hindi', 'turkish'];
+var mountains = ['mount everest', 'k2 mountain', 'karakaram', 'hindu kash', 'himalya'];
+var country = ['pakistan', 'china', 'africa', 'india', 'amerca'];
+var cities = ['kaarchi', 'multan', 'islamabad', 'lahore', 'rahim yar khan'];
+var river = ['karli jheel', 'neelam jheel', 'ravi', 'indus', 'jhelum'];
+//languages
+console.log(languages[0]);
+console.log(languages[2]);
+console.log(languages[2]);
+console.log(languages[3]);
+console.log(languages[4]);
+//mountain
+console.log(mountains[0]);
+console.log(mountains[1]);
+console.log(mountains[2]);
+console.log(mountains[3]);
+console.log(mountains[4]);
+//cities
+console.log(cities[0]);
+console.log(cities[1]);
+console.log(cities[2]);
+console.log(cities[3]);
+console.log(cities[4]);
+//river
+console.log(river[0]);
+console.log(river[1]);
+console.log(river[2]);
+console.log(river[3]);
+console.log(river[4]);
---
+++ Assignment 19/tech.ts
@@ -0,0 +1,49 @@
+//Think of something you could store in a array.
+// For example, you could make a list of mountains, rivers, countries, 
+//cities, languages, or anything else you’d like. Write a program that 
+//creates a list containing these items.
+
+let languages:string[]=['urdu' , 'english' , 'chines' ,'hindi' , 'turkish']
+let mountains:string[]=['mount everest' , 'k2 mountain' ,'karakaram','hindu kash' , 'himalya']
+let country:string[]=['pakistan' , 'china', 'africa' , 'india' ,'amerca']
+let cities:string[]=['kaarchi' , 'multan' ,'islamabad' , 'lahore' , 'rahim yar khan' ]
+let river:string[]=['karli jheel' , 'neelam jheel' , 'ravi' , 'indus', 'jhelum']
+
+//languages
+console.log(languages[0])
+console.log(languages[2])
+console.log(languages[2])
+console.log(languages[3])
+console.log(languages[4])
+
+//mountain
+console.log(mountains[0])
+console.log(mountains[1])
+console.log(mountains[2])
+console.log(mountains[3])
+console.log(mountains[4])
+ 
+ //cities
+console.log(cities[0])
+console.log(cities[1])
+console.log(cities[2])
+console.log(cities[3])
+console.log(cities[4])
+
+//river
+console.log(river[0])
+console.log(river[1])
+console.log(river[2])
+console.log(river[3])
+console.log(river[4])
+
+
+
+
+
+
+
+
+
+
+
