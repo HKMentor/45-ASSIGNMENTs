80d3aeb87a5b6bd2f8e799579449c62575c9da0d
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-03-14 23:53:28 +0500

    question 22

---
+++ Assignment 22/tech.js
@@ -0,0 +1,49 @@
+//Conditional Tests: Write a series of conditional tests. 
+//Print a statement describing each test and your prediction
+// for the results of each test. Your code should look something like this:
+//let car = 'subaru';
+//console.log("Is car == 'subaru'? I predict True.")
+//console.log(car == 'subaru')
+//• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+// 5 tests evaluate to True.
+//test 1
+var num = 7;
+console.log('is num == 7?i predict true.');
+console.log(num == 7);
+//test 2
+var name1 = 'hooria';
+console.log('is name == hooria?i predict true.');
+console.log(name1 == 'hooria');
+//test 3 
+var color = 'blue';
+console.log('is name == blue? i predict true.');
+console.log(color == 'blue');
+//test 4 
+var x = 100;
+console.log('is x == 100?i predict true.');
+console.log(x == 100);
+//test 5
+var car = 'cultus';
+console.log('is car == cultus? i predict true.');
+console.log(car == 'cultus');
+//test 6 
+var name2 = 'kanwal';
+console.log('is name2 ===kanal? i predict false.');
+console.log(name2 == 'kanal');
+//test 7
+var obj = 'key';
+console.log('is obj ==keys?i predict false.');
+console.log(obj == 'keys');
+//test 8
+var str = 'hello';
+console.log('is str == hellos?i predict false.');
+console.log(str == 'hellos');
+//test 9
+var mobile = 'realme c21';
+console.log('is mobile1 == realme c21?i predict false.');
+console.log(mobile == 'realme c21');
+//test 10 
+var shirt = 't-Shirt';
+console.log('is shirt == t-shirts? i predict false.');
+console.log(shirt == 't-shirts');
---
+++ Assignment 22/tech.ts
@@ -0,0 +1,54 @@
+//Conditional Tests: Write a series of conditional tests. 
+//Print a statement describing each test and your prediction
+// for the results of each test. Your code should look something like this:
+//let car = 'subaru';
+
+//console.log("Is car == 'subaru'? I predict True.")
+
+//console.log(car == 'subaru')
+
+//• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+// 5 tests evaluate to True.
+
+//test 1
+let num:number=7;
+console.log('is num == 7?i predict true.');
+console.log(num==7)
+//test 2
+let name1:string='hooria'
+console.log('is name == hooria?i predict true.')
+console.log(name1 == 'hooria')
+//test 3 
+let color='blue'
+console.log('is name == blue? i predict true.')
+console.log(color == 'blue')
+//test 4 
+let x:number = 100
+console.log('is x == 100?i predict true.')
+console.log(x == 100)
+//test 5
+let car= 'cultus'
+console.log('is car == cultus? i predict true.')
+console.log(car == 'cultus')
+//test 6 
+let name2:string='kanwal'
+console.log('is name2 ===kanal? i predict false.')
+console.log(name2 == 'kanal' ) 
+//test 7
+let obj='key'
+console.log('is obj ==keys?i predict false.')
+console.log(obj == 'keys')
+//test 8
+let str:string='hello'
+console.log('is str == hellos?i predict false.')
+console.log(str =='hellos')
+//test 9
+let mobile:any= 'realme c21'
+console.log('is mobile1 == realme c21?i predict false.')
+console.log(mobile == 'realme c21')
+//test 10 
+let shirt:any= 't-Shirt'
+console.log('is shirt == t-shirts? i predict false.')
+console.log(shirt == 't-shirts')
