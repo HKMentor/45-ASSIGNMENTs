41bccdc0cea1b4b5a78fd37fa73b8906a312c140
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-03-14 23:38:56 +0500

    question13

---
+++ Assignment 13/tech.js
@@ -0,0 +1,8 @@
+//Guest List: If you could invite anyone, living or deceased, to dinner, 
+//who would you invite? Make a list that includes at least
+// three people you’d like to invite to dinner. 
+//Then use your list to print a message to each person, inviting them to dinner.
+var guestlist = ["NEHA", "KANWAL", "RAHEELA"];
+console.log(guestlist[0] + ":i would be honored if you could join me for dinner.");
+console.log(guestlist[1] + ":i would be honored if you could join me for dinner.");
+console.log(guestlist[2] + ":i would be honored if you could join me for dinner.");
