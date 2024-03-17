11f7a8a2a28d61ac05819655e4cae1aa71d4a429
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-03-18 02:36:31 +0500

    question 29

---
+++ Assignment 29/tech.ts
@@ -0,0 +1,16 @@
+//Hello Admin: Make a array of five or more usernames, including the name 'admin'.
+// Imagine you are writing code that will print a greeting to each user after they log in to a website.
+// Loop through the array, and print a greeting to each user:
+//• If the username is 'admin', print a special greeting, such as Hello admin,
+// would you like to see a status report?
+
+//• Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+
+let UserName = ["Hooria","Neha","Kanwal","Raheela","Mehak","Fatima"]
+for(let H = 0; H <= 0; H++) {
+    console.log(UserName [0]  + " Hello admin, would you like to see the a status report")
+      console.log(UserName[1] + " Hello, thank you for logging in again.")
+     console.log(UserName[2] + " Hello, thank you for logging in again.")
+     console.log(UserName[3] + " Hello, thank you for logging in again.")
+     console.log(UserName[4] + " Hello, thank you for logging in again.")
+      console.log(UserName[5] + " Hello, thank you for logging in again.")}
